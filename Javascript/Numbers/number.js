b68beea2753021883beafd98cf512a4da0b41d4e
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-04-04 13:30:33 +0530

    code pushed for 4th april 2024

---
+++ Javascript/Numbers/number.js
@@ -0,0 +1,137 @@
+
+
+let a =10;
+
+// number system - octal binary decimal hexa 
+// custom -
+
+console.log(a.toString(8))
+console.log(a.toString(2));
+console.log(a.toString(16));
+
+console.log(a.toString(5));
+let rv=a.toString(8);
+console.log(rv);
+
+
+
+//rounding = floor ceil trunc  tofix
+
+let x=10.5666666;
+console.log(Math.floor(x));
+console.log(Math.ceil(x));
+console.log(Math.trunc(x));
+console.log(x.toFixed(3));
+
+//random
+
+console.log(Math.floor((Math.random()*100)));
+
+let rv2 =Math.random();
+console.log(rv2);
+
+// imprecison calculaiton 
+
+// console.log(10+40+true-false+20);
+
+
+// string 
+
+
+// let str="vikas";
+// console.log(str[2]);
+// console.log(str[10]);
+// str[1]="z";
+// console.log(str);
+
+
+
+
+
+
+// let arr=[10,20,30,40]
+// arr[1]=100;
+// console.log(arr);
+
+
+
+
+
+
+//string 
+
+let name="vikas";
+// console.log(name.length);
+// console.log(name.charAt(10));
+// console.log(name.charAt(15));
+//string slice (star,end)- start- inlcusive ,end =exclusive
+
+
+// console.log(name.slice(2,9));
+// console.log(name.slice(1));
+// // console.log(name.slice(,8));
+
+// console.log(name.slice(-4,-2));
+// console.log(name.slice(2,-1));
+// console.log(name.slice(-2,-5));
+
+
+//hw - 
+
+//substring , substr
+
+let name2="vikas chaudhary";
+console.log(name2.substring(2,8));
+console.log(name2.substring(-9,-5)); //negative index 
+
+console.log(name2.slice(-9,-5));
+
+console.log(name2.substr(-9,-5));
+
+
+
+//ascii code -
+let str2="\t";
+console.log(str2.charCodeAt(0));
+
+// let char="a";
+console.log(String.fromCharCode(97));
+
+/*
+
+A
+A B
+A B C
+A B C D
+A B C D E
+HINT - VAR=65
+
+
+ViKas ChauDhaRy
+
+q3 - upper- lower csee
+lower- upepr case'
+
+
+*/
+
+
+// - narsimha karumanchi dsa with c++
+// vikas k=2
+//asvik
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
