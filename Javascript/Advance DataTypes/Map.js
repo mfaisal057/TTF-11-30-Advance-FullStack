9627514bcd738f9808e140e4f1d275731493a8eb
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-04-11 13:26:08 +0530

    code pushed for 11th april 2024

---
+++ Javascript/Advance DataTypes/Map.js
@@ -0,0 +1,219 @@
+
+// // let map = new Map();
+// // // console.log(map);
+
+
+// // //map.size
+
+// // // console.log(map.size);
+
+// // //set
+// // map.set(1,10);
+// // map.set(2,20)
+// // map.set(3,30)
+// // map.set(4,40)
+// // map.set(4,50);
+// // console.log(map);
+// // console.log(map.size);
+
+// // //get
+
+// // console.log(map.get(2))
+// // console.log(map.get(50));
+
+// // //map.has -true false
+
+// // // console.log(map.has(40));
+
+
+// // //map.detele
+
+// // map.delete(3)
+// // console.log(map);
+
+// // //map.clear
+// // map.clear();
+// // console.log(map);
+
+// // //iterate
+// // // console.log(map.keys())
+
+// // let newmap= new Map();
+// // newmap.set(1,20);
+// // newmap.set(2,30)
+// // newmap.set(3,30)
+// // newmap.set(4,40)
+// // newmap.set(5,50)
+
+
+// // //iterate
+
+// // for(const value of newmap.keys()){
+// //     console.log(value);
+// // }
+
+// // //map.values()
+
+
+
+// // console.log(newmap.values());
+
+// // for(const value of newmap.values()){
+// //     console.log(value);
+// // }
+
+
+// // //key value pair - map.enterie()
+
+
+// // console.log(newmap.entries());
+// // for(const value of newmap.entries()){
+// //     console.log(value);
+// // }
+
+
+
+// // // set
+
+// // let set= new Set();
+// // // console.log(set);
+
+// // set.add(2)
+// // console.log(set);
+// // set.add(2)
+// // console.log(set);
+
+// // set.add([10,20,30])
+// // console.log(set);
+// // set.add([10,20,30]);
+// // console.log(set);
+// // set.add(2)
+// // console.log(set);
+
+
+
+// // //iterate
+
+// // console.log(set.size);
+
+// // //iterate - set.values
+
+// // console.log(set.values());
+// // for(const value of set.values()){
+// //     console.log(value);
+// // }
+
+// // //set delete ,clear
+
+// // set.delete(2);
+// // console.log(set);
+// // console.log(set.size);
+
+
+// // set.clear()
+// // console.log(set,set.size);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let obj={
+// //     a:10,
+// //     a:20,
+// // }
+// // console.log(obj);
+
+
+// // let weakmap= new WeakMap();
+// // console.log(weakmap);
+
+// //set
+
+// //  weakmap.set({key:1},10);
+// // weakmap.set({key:1},10);
+// // weakmap.set({key:1},10);
+// // console.log(weakmap);
+// // console.log(weakmap.size);
+
+// // //has 
+// // weakmap.has({})
+// // let obj={
+// //     key:1,
+// // }
+// // weakmap.set(obj,10);
+// // console.log(weakmap.has(obj));
+
+
+// // //delete
+// // let newwkmap= new WeakMap()
+
+// // let obj={
+// //     key:1,
+// // }
+
+// // newwkmap.set(obj,100);
+// // newwkmap.set({key:2},200)
+// // newwkmap.set({key:3},300)
+// // // newwkmap.delete(obj)
+// // console.log(newwkmap);
+
+// // //iterate-
+// // console.log(newwkmap.key());
+
+
+
+// // let obj={
+// //     key1:1,
+// //     key2:2,
+// //     key3:3,
+// //     key4:4,
+// // }
+
+// // wkmp.set(obj,1000)
+// // console.log(wkmp);
+
+// let obj={1:1}
+
+
+// let wkset= new WeakSet();
+// console.log(wkset);
+
+
+// wkset.add({1:10});
+// wkset.add({1:10});
+// wkset.add(obj)
+
+// console.log(wkset);
+// // obj=null;
+//  wkset.delete(obj)
+//  console.log("0000000000000000000");
+// console.log(wkset)
+
+// //has 
+// // console.log(wkset.has(obj))
+
+// // console.log(wkset);
+
+// //object hidden prorperties
+
+
+
+
+
+
+
+let newset = new Set()
+newset.add([10,20,30])
+newset.add([20,30,40,50])
+console.log(newset);
+
